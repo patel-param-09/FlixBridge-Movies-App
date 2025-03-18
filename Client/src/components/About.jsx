d028f0eb4010be2332d593359049ae940185222d
patel-param-09 <patelparam9304@gmail.com>
2025-03-18 14:47:20 +0530

    Add Back Button in About Page

--- Client/src/components/About.jsx
+++ Client/src/components/About.jsx
@@ -1,19 +1,32 @@
 import React from "react";
-import { useLocation, Link } from "react-router-dom";
+import { useLocation, Link, useNavigate } from "react-router-dom";
 import "../App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 
 function About() {
   const location = useLocation();
-  console.log(location);
+  const navigate = useNavigate();
   return (
-    <div className="div">
-      <h1 className="name">Name :- {location.state.name}</h1>
-      <h2 className="rating">Rating :- {location.state.rating}</h2>
-      <div className="link-div">
-        <span>Click This Below Button for more Details ....</span>
-        <Link to={location.state.url} target="_blank">
-          <button className="button">Open IMDb Page</button>
-        </Link>
+    <div>
+      <button
+        className="back-btn"
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        <FontAwesomeIcon icon={faAngleLeft} />
+        Back
+      </button>
+      <div className="div">
+        <h1 className="name">Name :- {location.state.name}</h1>
+        <h2 className="rating">Rating :- {location.state.rating}</h2>
+        <div className="link-div">
+          <span>Click This Below Button for more Details ....</span>
+          <Link to={location.state.url} target="_blank">
+            <button className="button">Open IMDb Page</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
